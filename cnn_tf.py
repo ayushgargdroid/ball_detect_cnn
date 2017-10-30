7252694b47f44e77d41ed109dc18036b7639625b
Ayush Garg <ayushgargdroid@gmail.com>
2017-10-31 00:51:36 +0530

    Tensorflow implementation fetching data

--- cnn_tf.py
+++ cnn_tf.py
@@ -1,15 +1,7 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Oct 28 18:36:26 2017
-
-@author: ayush
-"""
-
-import threading
+import multiprocessing as mp
 import numpy as np
-import tensorflow as tf
 import os
+import tensorflow as tf
 import cv2
 from tensorflow.python.framework import ops
 
@@ -25,18 +17,14 @@ x_train2 = np.uint8([])
 y_train2 = np.uint8([])
 x_test = np.uint8([])
 y_test = np.uint8([])
-
-class DataThread(threading.Thread):
-    def __init__(self,sub,positive):
-        threading.Thread.__init__(self)
-        self.sub = sub
-        self.positive = positive
         
-    def run(self):
-        getData(self.sub,self.positive)
-        
-def getData(dataset_name,sub,positive):
+def getDataFlatten(dataset_name,sub,positive):
     path = '/home/ayush/ball_detect_cnn/'+dataset_name+'/'+sub
+    x_train = np.uint8([])
+    y_train = np.uint8([])
+    x_test = np.uint8([])
+    y_test = np.uint8([])
+    count=0
     if(sub=='training_set'):
         sub2 = os.listdir(path)
         for inn in sub2:
@@ -50,35 +38,29 @@ def getData(dataset_name,sub,positive):
                 print('train '+i)
                 if(i.find('jpg')==-1):
                     continue
-                if(i.find('no')==-1 and positive==1):
-                    y_train1 = np.append(y_train1,1)
-                elif(i.find('no')==-1 and positive==0):
-                    y_train2 = np.append(y_train2,1)
-                elif(positive==1):
-                    y_train1 = np.append(y_train1,0)
-                elif(positive==0):
-                    y_train2 = np.append(y_train2,0)
-                img = cv2.imread(path+'/'+i,cv2.IMREAD_COLOR).flatten()
-                print(img.shape)
-                if(len(x_train1.shape) is not 1):
-                    img.shape = (1,691200)
-                if(len(x_train2.shape) is not 1):
-                    img.shape = (1,691200)
-                print(img.shape)
-                if(positive==1):
-                    x_train1 = np.append(x_train1,img,axis=0)
+                if(i.find('no')==-1):
+                    y_train = np.append(y_train,1)
                 else:
-                    x_train2 = np.append(x_train2,img,axis=0)
-                if(len(x_train1.shape) is 1):
-                    x_train1.shape = (1,x_train1.shape[0])
-                if(len(x_train2.shape) is 1):
-                    x_train2.shape = (1,x_train2.shape[0])
+                    y_train = np.append(y_train,0)
+                img = cv2.imread(path+'/'+i,cv2.IMREAD_COLOR).flatten()
+                if(len(x_train.shape) is not 1):
+                    img.shape = (1,img.shape[0])
+                print(img.shape)
+                x_train = np.append(x_train,img,axis=0)
+                if(len(x_train.shape) is 1):
+                    x_train.shape = (1,x_train.shape[0])
+                count+=1
+                print(count)
+        y_train.shape = (y_train.shape[0],1)
+        print('sdas')
         if(positive==1):
-            y_train1.shape = (y_train1.shape[0],1)
+            np.save('x1',x_train)
+            np.save('y1',y_train)
         else:
-            y_train2.shape = (y_train2.shape[0],1)
+            np.save('x2',x_train)
+            np.save('y2',y_train)
     
-    if(sub=='test_set'):
+    elif(sub=='test_set'):
         path = '/home/ayush/ball_detect_cnn/'+dataset_name+'/'+sub
         sub2 = os.listdir(path)
         for inn in sub2:
@@ -101,31 +83,168 @@ def getData(dataset_name,sub,positive):
                 if(len(x_test.shape) is 1):
                     x_test.shape = (1,x_test.shape[0])
         y_test.shape = (y_test.shape[0],1)
-        print('X_test: ',x_test.shape)
-        print('Y_test: ',y_test.shape)    
+        np.save('x3',x_test)
+        np.save('y3',y_test)
 
-train_thread1 = DataThread('training_set',1)
-train_thread2 = DataThread('training_set',0)
-test_thread = DataThread('test_set',0)
-
-try:
-    train_thread1.start()
-    train_thread2.start()
-    test_thread.start()
-    test_thread.join()
-    train_thread1.join()
-    train_thread2.join()
-except:
-    print('Error')
+def getData(dataset_name,sub,positive):
+    path = '/home/ayush/ball_detect_cnn/'+dataset_name+'/'+sub
+    x_train = np.uint8([])
+    y_train = np.uint8([])
+    x_test = np.uint8([])
+    y_test = np.uint8([])
+    count=0
+    if(sub=='training_set'):
+        sub2 = os.listdir(path)
+        for inn in sub2:
+            if(positive==1 and inn=='noball'):
+                continue
+            elif(positive==0 and inn=='ball'):
+                continue
+            path = '/home/ayush/ball_detect_cnn/'+dataset_name+'/'+sub+'/'+inn
+            imgs = os.listdir(path)
+            for i in imgs:
+                print('train '+i)
+                if(i.find('jpg')==-1):
+                    continue
+                if(i.find('no')==-1):
+                    y_train = np.append(y_train,1)
+                else:
+                    y_train = np.append(y_train,0)
+                img = cv2.imread(path+'/'+i,cv2.IMREAD_COLOR)
+                if(len(x_train.shape) is 1):
+                    x_train = np.uint8([img])
+                else:
+                    x_train = np.append(x_train,[img],axis=0)
+                count+=1
+                print(count)
+        y_train.shape = (y_train.shape[0],1)
+        if(positive==1):
+            np.save('x1',x_train)
+            np.save('y1',y_train)
+        else:
+            np.save('x2',x_train)
+            np.save('y2',y_train)
     
-x_train = x_train1
+    elif(sub=='test_set'):
+        path = '/home/ayush/ball_detect_cnn/'+dataset_name+'/'+sub
+        sub2 = os.listdir(path)
+        for inn in sub2:
+            path = '/home/ayush/ball_detect_cnn/'+dataset_name+'/'+sub+'/'+inn
+            imgs = os.listdir(path)
+            for i in imgs:
+                print('test '+i)
+                if(i.find('jpg')==-1):
+                    continue
+                if(i.find('no')==-1):
+                    y_test = np.append(y_test,1)
+                else:
+                    y_test = np.append(y_test,0)
+                img = cv2.imread(path+'/'+i,cv2.IMREAD_COLOR)
+                if(len(x_test.shape) is 1):
+                    x_test = np.uint8([img])
+                else:
+                    x_test = np.append(x_test,[img],axis=0)
+        y_test.shape = (y_test.shape[0],1)
+        np.save('x3',x_test)
+        np.save('y3',y_test)
+
+
+#Shuffle Data        
+def getShuffled(x,y):
+    shuffler = np.arange(x.shape[0])
+    np.random.shuffle(shuffler)
+    x = np.uint8([x[shuffler[i]] for i in range(x.shape[0])])
+    y = np.uint8([y[shuffler[i]] for i in range(y.shape[0])])
+    del shuffler
+    return x,y
+
+def getWeight(shape):
+    initial = tf.Variable(tf.truncated_normal(shape,stddev=0.1))
+    return initial
+
+def getBias(shape):
+    initial = tf.Variable(tf.constant(0.1,shape=shape))
+    return initial
+
+def getWeights(a,b,i):
+    W = tf.get_variable('W'+str(i),[b,a],initializer=tf.contrib.layers.xavier_initializer())
+    c = tf.get_variable('b'+str(i),[b,1],initializer=tf.zeros_initializer())
+    return W,c
+
+train1_process = mp.Process(target=getData,args=(dataset_name,'training_set',1))
+train2_process = mp.Process(target=getData,args=(dataset_name,'training_set',0))
+test_process = mp.Process(target=getData,args=(dataset_name,'test_set',2))
+
+train1_process.start()
+train2_process.start()
+test_process.start()
+train1_process.join()
+train2_process.join()
+test_process.join()
+
+x_train1 = np.load('x1.npy')
+y_train1 = np.load('y1.npy')
+x_train2 = np.load('x2.npy')
+y_train2 = np.load('y2.npy')
+x_test = np.load('x3.npy')
+y_test = np.load('y3.npy')
+
+np.save('x1',x_train1)
+np.save('y1',y_train1)
+np.save('x2',x_train2)
+np.save('y2',y_train2)
+np.save('x3',x_test)
+np.save('y3',y_test)
+
 x_train = np.append(x_train1,x_train2,axis=0)
-y_train = y_train1
 y_train = np.append(y_train1,y_train2,axis=0)
+del x_train1,x_train2,y_train1,y_train2
+
+x_train,y_train = getShuffled(x_train,y_train)
+x_test,y_test = getShuffled(x_test,y_test)
+
 print('X_train: ',x_train.shape)
 print('Y_train: ',y_train.shape)
 print('X_test: ',x_test.shape)
 print('Y_test: ',y_test.shape)
-            
-            
+
+minibatch_size = 32
+epochs = 5
+m = x_train.shape[0]
+dimen = x_train.shape[1:]
+#batches = m/minibatch_size
+batches = 5
+
+X = tf.placeholder(tf.float32,shape=x_train.shape,name='X')
+Y = tf.placeholder(tf.float32,shape=(m,1),name='Y')
+
+for i in range(batches):
+    W1,b1 = getWeight([5,5,1,32]),getBias([32])
+    conv = tf.nn.relu(tf.nn.conv2d(X,W1)+b1)
+    pool = tf.nn.max_pool(conv,ksize=[1,2,2,1],strides=[1,2,2,1],padding='same')
+    W2,b2 = getWeight([5,5,32,64]),getBias([64])
+    conv = tf.nn.relu(tf.nn.conv2d(pool,W2)+b2)
+    pool = tf.nn.max_pool(conv,ksize=[1,2,2,1],strides=[1,2,2,1],padding='same')
+    X = tf.reshape(pool,[-1,dimen[0]*dimen[1]*dimen[2]])
+    W3,b3 = getWeight([dimen[0]*dimen[1]*dimen[2],256]),getBias([256])
+    X = tf.nn.relu(tf.matmul(X,W3)+b3)
+    W4,b4 = getWeight([256,1]),getBias([1])
+    X = tf.matmul(X,W3)+b3
+    
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = tf.transpose(X), labels = tf.transpose(Y))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.0001).minimize(cost)
+epochs = 10
+sess = tf.Session()
+for epoch in range(epochs):
+    epoch_cost = 0
+    for i in range(batch):
+        (miniX,miniY) = x_train[i*32:(i+1)*32],y_train[i*32:(i+1)*32]
+        _ , minibatch_cost = sess.run([optimizer, cost], feed_dict={X: miniX, Y: miniY})
+        epoch_cost += minibatch_cost/batch
+    print('Cost after epoch %i is %f' % (epoch+1,epoch_cost))
+
+
+    
+    
+    
             
