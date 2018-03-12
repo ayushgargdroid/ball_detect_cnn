a04f3620d7eda61898f0357d080f4e36191e625d
ayushgargdroid <ayushgargdroid@gmail.com>
2018-03-12 22:48:17 +0530

    Debug

--- cnn_tries/mnist_style_classifier.py
+++ cnn_tries/mnist_style_classifier.py
@@ -7,7 +7,6 @@ import time
 from datetime import timedelta
 import os
 import cv2
-import cv2.cv as cv
 
 # dataset_name = 'labelled_dataset1'
 dataset_name = 'dataset3'
@@ -215,15 +214,6 @@ def optimize(num_iterations):
                 frame = cv2.resize(frame,(200,200))
                 # tt = frame.copy()
                 t = frame.copy()
-                # cimg = cv2.cvtColor(tt,cv2.COLOR_BGR2GRAY)
-                # img = cv2.medianBlur(cimg,5)
-                # circles = cv2.HoughCircles(img,cv.CV_HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
-                # circles = np.uint16(np.around(circles))
-                # for i in circles[0,:]:
-                #     # draw the outer circle
-                #     cv2.circle(t,(i[0],i[1]),i[2],(0,255,0),2)
-                #     # draw the center of the circle
-                #     cv2.circle(t,(i[0],i[1]),2,(0,0,255),3)
                 frame.shape = (1,200,200,3)
                 pred = session.run(y_pred_cls,feed_dict={x:frame,keep_prob: 1.0})
                 cv2.imshow('Image',t)
